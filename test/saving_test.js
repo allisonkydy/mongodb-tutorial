3a2ddf9cfe77b7608ce9dab435a0bbb5e70f3820
allisonkydy <allisnelly@gmail.com>
2019-11-17 19:35:15 -0600

    add saving test

---
+++ test/saving_test.js
@@ -0,0 +1,24 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+describe('Saving records', () => {
+
+  it('Saves a record to the database', (done) => {
+    
+    // create a new mario character
+    const char = new MarioChar({
+      name: 'Mario'
+    });
+
+    // save character to database -- mongoose knows where to save
+    char.save()
+      .then(() => {
+        // true if not saved to db, false if saved
+        assert(char.isNew === false);
+        // tells mocha that test is done
+        done();
+      });
+  });
+
+});
